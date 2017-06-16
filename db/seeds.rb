fc7563cb349331390f7f705371c514f2adb139f0
MASHA <mashanikit@gmail.com>
2017-06-16 15:38:17 -0400

    all restaurants page index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+joeys = Restaurant.create(name: "Joeys", user_id: 1, neighbourhood: "Yonge and Dundas", address: "123 Yonge Street", cuisine_type: "American", opens: 11, closes: 23, capacity: 200)
+
+cactus_club = Restaurant.create(name: "Cactus Club", user_id: 2, neighbourhood: "Financial District", address: "123 King Street West", cuisine_type: "Asian/American", opens: 12, closes: 23, capacity: 100)
