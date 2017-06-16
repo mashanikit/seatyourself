b1035a71184de97cd6c300540566176219998ec9
MASHA <mashanikit@gmail.com>
2017-06-16 15:18:19 -0400

    added index to show all restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,10 @@
 class Restaurant < ApplicationRecord
   has_many :reservations
+
+  validates :name, presence: true
+  validates :neighbourhood, presence: true
+  validates :address, presence: true
+  validates :cuisine_type, presence: true
+
   
 end
