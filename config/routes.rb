9b2468eaaa56229393de570ed4456fedc463ca92
Blueibis <jennie.li.2011@gmail.com>
2017-06-16 14:34:27 -0400

    things from friday lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root 'home#index'
+  resource :users, only: [:new, :create]
+  resource :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
