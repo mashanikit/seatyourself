54cdde2fe9a06dc0c010733cbaa792a0ce41c1a6
MASHA <mashanikit@gmail.com>
2017-06-18 17:01:10 -0400

    created validation for users not being able to create reso if restaurant is not open

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,12 +6,30 @@ class Reservation < ApplicationRecord
 
   validates :size, numericality: true
 
+  validate :reso_in_hours
+
+
   validate :reso_date_not_in_past
+
 
   def reso_date_not_in_past
     if date.present? && date < Date.today
       errors.add(:date, "Can't be in the Past")
     end
   end
+
+  def reso_in_hours
+      if self.time < self.restaurant.opens
+        errors.add(:time, "Restaurant is not open")
+      else self.time > self.restaurant.closes
+        errors.add(:time, "Restaurant is not open")
+      end
+  end
+
+
+
+
+
+
 
 end
